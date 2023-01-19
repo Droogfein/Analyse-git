e1d80eb2b9486a2d198ba7f6a41b3f66cef35484
draugfein <arndraugfein@gmail.com>
2023-01-19 06:35:31 +0100

    Added word cloud for the netflix countries

--- analyse.py
+++ analyse.py
@@ -14,16 +14,30 @@
 import seaborn as sb
 import matplotlib.pyplot as plt
 import pandas as pd
+from wordcloud import WordCloud
 
 # .csv Tabelle mit Daten in das Pandas-DataFrame df ablegen
-df = pd.read_csv('Attendance.csv')
+df1 = pd.read_csv('Attendance.csv')
 
-sb.countplot(x = "Home" , data=df, order = df['Home'].value_counts().index)
-plt.title("Mannschaften nach Anzahl der gespielten Fußballspiele")
+p1 = plt.figure(1)
+
+sb.countplot(x = "Home" , data=df1, order = df1['Home'].value_counts().index)
+plt.title("Mannschaften nach Anzahl der gespielten Fußballspiele als Heimmannschaft")
 plt.xlabel("Mannschaften")
 plt.ylabel("Anzahl der Spiele")
 plt.xticks(rotation=90)
-plt.show()
 
-sb.jointplot(data=df, x="Home", y= "Away", kind="hex")
+df2 = pd.read_csv('NetFlix.csv')
+
+wc = WordCloud(
+    background_color="white", 
+    max_words=len(df2['country']), 
+    max_font_size=38, 
+    relative_scaling=0.4
+).generate(" ".join(str(country) for country in df2['country']))
+
+plt.figure()
+plt.imshow(wc)
+plt.axis("off")
+plt.tight_layout()
 plt.show()
