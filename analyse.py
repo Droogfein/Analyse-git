3050930be9bf60bccf5cbbc2cdb138f55b72b5ab
draugfein <arndraugfein@gmail.com>
2023-01-19 07:27:14 +0100

    Fixed WordCloud resolution

--- analyse.py
+++ analyse.py
@@ -5,7 +5,7 @@
 ## auf die Anzahl der Besucher für die jeweiligen
 ##
 ## Der Datensatz wurde von Kaggle übernommen:
-## https://www.kaggle.com/datasets/parasharmanas/qatar-2022-fifa-world-cup-attendance?resource=download
+## https://www.kaggle.com/
 ##
 ## Außerdem werden verschiedene Zusammenhänge und Gruppierungen
 ## visuell dargestellt mit hilfe des Seaborn Pakets/Bibliothek.
@@ -19,25 +19,25 @@ from wordcloud import WordCloud
 # .csv Tabelle mit Daten in das Pandas-DataFrame df ablegen
 df1 = pd.read_csv('Attendance.csv')
 
-p1 = plt.figure(1)
-
 sb.countplot(x = "Home" , data=df1, order = df1['Home'].value_counts().index)
 plt.title("Mannschaften nach Anzahl der gespielten Fußballspiele als Heimmannschaft")
 plt.xlabel("Mannschaften")
 plt.ylabel("Anzahl der Spiele")
 plt.xticks(rotation=90)
+plt.tight_layout()
 
 df2 = pd.read_csv('NetFlix.csv')
 
 wc = WordCloud(
     background_color="white", 
     max_words=len(df2['country']), 
-    max_font_size=38, 
-    relative_scaling=0.4
+    max_font_size=100, 
+    relative_scaling=.4
 ).generate(" ".join(str(country) for country in df2['country']))
 
 plt.figure()
 plt.imshow(wc)
+plt.title("World cloud der Länder wo Netflix FIlme und Serien aufgenommen wurden.")
 plt.axis("off")
 plt.tight_layout()
 plt.show()
